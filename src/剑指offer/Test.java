b334b9a9ba77d65af05108309a942ecf2aaa0beb
belief.cn <673727003@qq.com>
2020-07-10 23:32:51 +0800

    从尾到头打印链表/Main.java

---
+++ src/剑指offer/Test.java
@@ -0,0 +1,8 @@
+package 剑指offer;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "123456";
+        System.out.println(str.length());
+    }
+}
--- src/剑指offer/从尾到头打印链表/Main.java
+++ src/剑指offer/从尾到头打印链表/Main.java
@@ -5,22 +5,42 @@ import OneQuestionEveryDay.Package_07.链表分割.ListNode;
 import java.util.ArrayList;
 
 public class Main {
+
+        ArrayList<Integer> list = new ArrayList<>();
         public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
             // 头插法构建逆序链表
-            ListNode head = new ListNode(-1);
-            while (listNode != null) {
-                ListNode memo = listNode.next;
-                listNode.next = head.next;
-                head.next = listNode;
-                listNode = memo;
+            ListNode dummyHead = new ListNode(-1);
+            while (listNode != null){
+                ListNode s = listNode.next;
+                listNode.next = dummyHead.next;
+                dummyHead.next = listNode;
+                listNode = s;
             }
-            // 构建 ArrayList
-            ArrayList<Integer> ret = new ArrayList<>();
-            head = head.next;
-            while (head != null) {
-                ret.add(head.val);
-                head = head.next;
+            ArrayList<Integer> list = new ArrayList<>();
+            dummyHead = dummyHead.next;
+            while (dummyHead != null){
+                list.add(dummyHead.val);
+                dummyHead = dummyHead.next;
             }
-            return ret;
+            return list;
         }
+
+    /**
+     * 递归
+     * @param listNode
+     * @return
+     */
+    public ArrayList<Integer> PrintListFromTailToHead(ListNode listNode){
+        recur(listNode);
+        return list;
+    }
+
+    private void recur(ListNode listNode){
+        if(listNode == null){
+            return;
+        }
+        recur(listNode.next);
+        list.add(listNode.val);
+    }
+
 }
