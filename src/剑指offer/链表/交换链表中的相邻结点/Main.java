1ff524bcc138bfdd4ca864e4b2a02211ffc4889a
belief.cn <673727003@qq.com>
2020-07-14 22:50:49 +0800

    交换链表中的相邻结点/Main.java

---
+++ src/剑指offer/链表/交换链表中的相邻结点/Main.java
@@ -0,0 +1,30 @@
+package 剑指offer.链表.交换链表中的相邻结点;
+
+import OneQuestionEveryDay.Package_07.链表分割.ListNode;
+
+public class Main {
+    /**
+     * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+     *
+     * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     *
+     * 交换链表的相邻结点
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+        ListNode node = new ListNode(-1);
+        node.next = head;
+        ListNode pre = node;
+        while (pre.next != null && pre.next.next != null) {
+            ListNode l1 = pre.next, l2 = pre.next.next;
+            ListNode next = l2.next;
+            l1.next = next;
+            l2.next = l1;
+            pre.next = l2;
+
+            pre = l1;
+        }
+        return node.next;
+    }
+}
