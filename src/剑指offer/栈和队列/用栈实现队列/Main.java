8587d5da3ba2d664315975eef45425e19ad3e789
belief.cn <673727003@qq.com>
2020-07-16 22:27:51 +0800

    用栈实现队列/Main.java

---
+++ src/剑指offer/栈和队列/用栈实现队列/Main.java
@@ -0,0 +1,40 @@
+package 剑指offer.栈和队列.用栈实现队列;
+
+import java.util.Stack;
+
+
+/*
+用栈实现队列
+两个队列   
+ */
+public class Main {
+        private Stack<Integer> in = new Stack<>();
+        private Stack<Integer> out = new Stack<>();
+
+        public void push(int x) {
+            in.push(x);
+        }
+
+        public int pop() {
+            in2out();
+            return out.pop();
+        }
+
+        public int peek() {
+            in2out();
+            return out.peek();
+        }
+
+        private void in2out() {
+            if (out.isEmpty()) {
+                while (!in.isEmpty()) {
+                    out.push(in.pop());
+                }
+            }
+        }
+
+        public boolean empty() {
+            return in.isEmpty() && out.isEmpty();
+        }
+
+}
