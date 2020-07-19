141b9491bed2fb6587c8512ab6b2eb8e135ed1c9
belief.cn <673727003@qq.com>
2020-07-19 23:29:52 +0800

    反转链表2/Main.java

---
+++ src/剑指offer/链表/反转链表2/Main.java
@@ -0,0 +1,41 @@
+package 剑指offer.链表.反转链表2;
+
+import OneQuestionEveryDay.Package_07.链表分割.ListNode;
+
+/*
+反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+
+说明:
+1 ≤ m ≤ n ≤ 链表长度。
+
+示例:
+
+输入: 1->2->3->4->5->NULL, m = 2, n = 4
+输出: 1->4->3->2->5->NULL
+
+ */
+public class Main {
+    /**
+     * @param head
+     * @param m
+     * @param n
+     * @return
+     */
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        for(int i = 1; i < m; i++){
+            pre = pre.next;
+        }
+        head = pre.next;
+        for(int i = m; i < n; i++){
+            ListNode nex = head.next;
+            head.next = nex.next;
+            nex.next = pre.next;
+            pre.next = nex;
+        }
+        return dummy.next;
+
+    }
+}
